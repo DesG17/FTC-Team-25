f58af3f2cbfc086188b156780ff995d4d5108d01
Desiree <12desg22@gmail.com>
2020-11-02 20:39:11 -0800

    update SDK and fixed build issues

--- 2020/test/FourWheelDeadReckonPath.java
+++ 2020/test/FourWheelDeadReckonPath.java
@@ -43,7 +43,7 @@ import team25core.FourWheelDirectDrivetrain;
 import team25core.Robot;
 import team25core.RobotEvent;
 
-@Autonomous(name = "DeadReckonExample")
+@Autonomous(name = "FourWheelDeadReckonPath")
 //@Disabled
 public class FourWheelDeadReckonPath extends Robot {
 
